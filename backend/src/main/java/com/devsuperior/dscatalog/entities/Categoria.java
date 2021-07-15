95655115915a1b216e37f4e91a1494aa7be5eb64
AglayrtonUECE <86808209+AglayrtonUECE@users.noreply.github.com>
2021-07-14 23:29:50 -0300

    Criado a entidade Categoria

---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Categoria.java
@@ -0,0 +1,73 @@
+package com.devsuperior.dscatalog.entities;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable{
+	/**
+	 * É uma interface que faz com que o objeto java possa ser convertido em bytes
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String nome;
+	
+	public Categoria() {
+		
+	}
+
+	public Categoria(Long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	//método padrão de comparação (ma nao certeza absoluta)
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+	
+	//método padrão de comparação 100%
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+	
+}
