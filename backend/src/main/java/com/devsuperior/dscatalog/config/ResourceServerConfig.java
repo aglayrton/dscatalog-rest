65ea9771c004c1e76ac319ce8374865b1d28b11a
AglayrtonUECE <86808209+AglayrtonUECE@users.noreply.github.com>
2021-08-09 23:31:34 -0300

    Liberado o h2

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -14,10 +17,13 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
 	@Autowired
+	private Environment env; //é o ambiente de execução da aplicação, por eles conseguimos acessar várias partes
+	
+	@Autowired
 	private JwtTokenStore tokenStore;
 	
 	//declaraoms os vetores
-	private static final String[] PUBLIC = {"/oauth/token"};
+	private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
 	
 	private static final String[] OPERATOR_OR_ADMIN = {"/produtos/**", "/categorias/**"};
 	
@@ -33,6 +39,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	//configurar as rotas
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		
+		//para liberar o h2
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
 		//quem pode acesser o que
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
