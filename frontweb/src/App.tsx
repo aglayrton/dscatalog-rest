9b64132071ee011a9757976df271219405a873cf
AglayrtonUECE <86808209+AglayrtonUECE@users.noreply.github.com>
2021-10-10 15:29:48 -0300

    Navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'; //fica meio apagado pq nao é obrigatorio
 import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from 'components/Navbar';
 
 function App() {
   return (
-    <h1>Hello DSCatalog</h1>
+    <>
+      <Navbar />
+      <h1>Hello DSCatalog</h1>
+    </>
   );
 }
 
